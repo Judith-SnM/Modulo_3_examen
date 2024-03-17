c4206eba1a003e157dfb5748c6784b0c338c0ec4
Judith <judith.sanmartin.s@gmail.com>
2024-03-17 02:02:16 -0300

    tarjeta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,86 @@
-let superheroe = document.querySelector(`#formulario`)
-const numSuperHero = document.querySelector(`#numSuperHero`)
-const errorSuperHero = document.querySelector(`.errorSuperHero`)
-const regex1 = /[0-9]/gim
+$(document).ready(function () {
+    const superheroe = $(`#formulario`)
+    const numSuperHero = $(`#numSuperHero`)
+    const resulhero = $(`#resulhero`)
 
-formulario.addEventListener("submit", (event) => {
-    event.preventDefault()
-    if (numSuperHero.value.trim() === "") {
-        errorSuperHero.textContent = ("El Numero es requerido");
-    } else {
-        if (regex1.test(numSuperHero.value)) {
+    superheroe.on("submit", function (event) {
+        event.preventDefault()
+        numSuperHero.removeClass("is-valid is-invalid")
+
+        const usernumSuperHero = +numSuperHero.val()
+        if (usernumSuperHero > 0) {
+            if (usernumSuperHero < 731) {
+                numSuperHero.addClass("is-valid")
+                getSuperHero(usernumSuperHero)
+            } else {
+                numSuperHero.addClass("is-invalid")
+            }
+
         } else {
-            errorSuperHero.textContent = ("error con la informacion entregada, solo Numeros");
+            numSuperHero.addClass("is-invalid")
         }
+
+    })
+
+
+    const getSuperHero = (numSuperHerofn) => {
+        $.ajax({
+            url: `https://www.superheroapi.com/api.php/4905856019427443/${numSuperHerofn}`,
+            method: "GET",
+            success(SuperHero) {
+                const mySuperHero = {
+                    Imagen: SuperHero.image.url,
+                    nombre: SuperHero.name,
+                    conexiones: SuperHero.connections.group_affiliation,
+                    publicado: SuperHero.biography.publisher,
+                    ocupacion: SuperHero.work.occupation,
+                    aparicion: SuperHero.biography.first_appearance,
+                    altura: SuperHero.appearance.height,
+                    peso: SuperHero.appearance.weight,
+                    alianza: SuperHero.biography.aliases,
+                }
+                resulhero.html(`
+                
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${mySuperHero.Imagen}" class="img-fluid rounded-start" alt="SuperHero">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Nombre: ${mySuperHero.nombre}</h5>
+                                <p class="card-text">Conexiones: ${mySuperHero.conexiones}</p>
+                                <div class="card-text" style="width: 18rem;">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item">Publicado por: ${mySuperHero.publicado} </li>
+                                        <li class="list-group-item">Ocupación: ${mySuperHero.ocupacion}</li>
+                                        <li class="list-group-item">Primera Aparición: ${mySuperHero.aparicion}</li>
+                                        <li class="list-group-item">Altura: ${mySuperHero.altura} </li>
+                                        <li class="list-group-item">Peso: ${mySuperHero.peso}</li>
+                                        <li class="list-group-item">Alianzas: ${mySuperHero.alianza} </li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                             
+                
+                `)
+
+
+            },
+            error(e) {
+                console.log(e)
+            }
+        })
+
     }
 
+
+
 })
+
+
+
